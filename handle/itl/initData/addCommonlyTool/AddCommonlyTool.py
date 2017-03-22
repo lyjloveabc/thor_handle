2c336986ed60cff65e792133b961d1c46ba841a9
luoyanjie <546223592@qq.com>
2017-03-22 10:59:45 +0800

    lyj    ok;

---
+++ handle/itl/initData/addCommonlyTool/AddCommonlyTool.py
@@ -0,0 +1,32 @@
+"""
+
+"""
+import re
+import xml.dom.minidom
+
+from utils.ExcelReadUtil import ExcelReadUtil
+
+__AUTHOR = 'thor'
+
+
+class AddCommonlyTool(object):
+    __BASE_PATH = 'file/'
+
+    def __init__(self):
+        self.sql = 'insert into role_permission_relation(gmt_create, gmt_modify, role_code, permission_code) values '
+
+    def handle(self):
+        print(self.sql)
+
+        with open(AddCommonlyTool.__BASE_PATH + 'role.txt') as f:
+            for line in f.readlines():
+                info_array = line[:-1].split(',')
+                print('(now(), now(), "' + info_array[3] + '", "releaseTask"),')
+                print('(now(), now(), "' + info_array[3] + '", "releaseMatter"),')
+                print('(now(), now(), "' + info_array[3] + '", "releaseRepair"),')
+        print(';')
+
+
+if __name__ == '__main__':
+    handle = AddCommonlyTool()
+    handle.handle()
