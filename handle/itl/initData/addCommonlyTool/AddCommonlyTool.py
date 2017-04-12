5b625c39ebaaa9218393c40231d9f472a9063c40
luoyanjie <546223592@qq.com>
2017-04-12 15:37:03 +0800

    k

--- handle/itl/initData/addCommonlyTool/AddCommonlyTool.py
+++ handle/itl/initData/addCommonlyTool/AddCommonlyTool.py
@@ -12,14 +12,20 @@ class AddCommonlyTool(object):
         self.sql = 'insert into role_permission_relation(gmt_create, gmt_modify, role_code, permission_code) values '
 
     def handle(self):
+        has_role_code_group = set()
+        with open(AddCommonlyTool.__BASE_PATH + 'has_role_code.txt', 'r') as f:
+            for line in f.readlines():
+                has_role_code_group.add(line[:-1])
+
         print(self.sql)
 
         with open(AddCommonlyTool.__BASE_PATH + 'role.txt') as f:
             for line in f.readlines():
                 info_array = line[:-1].split(',')
-                print('(now(), now(), "' + info_array[3] + '", "releaseTask"),')
-                print('(now(), now(), "' + info_array[3] + '", "releaseMatter"),')
-                print('(now(), now(), "' + info_array[3] + '", "releaseRepair"),')
+                if info_array[3] not in has_role_code_group:
+                    print('(now(), now(), "' + info_array[3] + '", "releaseTask"),')
+                    print('(now(), now(), "' + info_array[3] + '", "releaseMatter"),')
+                    print('(now(), now(), "' + info_array[3] + '", "releaseRepair"),')
         print(';')
 
 
