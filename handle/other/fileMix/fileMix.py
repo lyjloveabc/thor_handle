2580ce11f4c283faf3f3847f73da8a991b68a119
luoyanjie <546223592@qq.com>
2017-05-15 16:48:13 +0800

    lyj    自动化；

---
+++ handle/other/fileMix/fileMix.py
@@ -0,0 +1,45 @@
+"""
+二进制读取文件
+添加删除其中字节
+"""
+import os
+import logging
+
+from utils.constant.constant import Constant
+from utils.constant.resultCode import ResultCode
+from utils.system.systemUtil import SystemUtil
+
+
+class FileMix:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def handle(path=os.getcwd(), extension=None, go_deep=True):
+        # 获取指定目录下的所有文件和目录名
+        file_name_group = os.listdir(path)
+
+        for file_name in file_name_group:
+            # 连接目录与文件名或目录
+            old_full_file_name = os.path.join(path, file_name)
+
+            if extension is None or Constant.DOT not in old_full_file_name or old_full_file_name.split(Constant.DOT)[1] in extension:
+                is_file = os.path.isfile(old_full_file_name)
+                is_dir = os.path.isdir(old_full_file_name)
+
+                if is_file:
+                    with open(old_full_file_name, 'wb') as f:
+                        f.write(bytes('hahCP', encoding="utf8"))
+                elif is_dir and go_deep:
+                    FileMix.handle(path=old_full_file_name)
+                else:
+                    logging.error(ResultCode.CONDITION_NOT_CONFORM)
+        pass
+
+
+if __name__ == '__main__':
+    data = '/Users/luoyanjie/temp'
+
+    obj = FileMix()
+    obj.handle(data)
+    SystemUtil.batch_rename_file(data)
--- utils/system/systemUtil.py
+++ utils/system/systemUtil.py
@@ -39,6 +39,39 @@ class SystemUtil:
                 else:
                     logging.error(ResultCode.CONDITION_NOT_CONFORM)
 
+    @staticmethod
+    def batch_rename_file_2(path=os.getcwd(), extension=None, go_deep=True):
+        """
+        批量修改文件名
+        extension: 扩展名列表，只要在这个列表里面的才会被修改文件名
+        go_deep: 是否深入下一级目录修改文件名
+        """
+        # 获取指定目录下的所有文件和目录名
+        file_name_group = os.listdir(path)
+
+        index = 0
+        for file_name in file_name_group:
+            index += 1
+            # 连接目录与文件名或目录
+            old_full_file_name = os.path.join(path, file_name)
+
+            if extension is None or Constant.DOT not in old_full_file_name or old_full_file_name.split(Constant.DOT)[1] in extension:
+                is_file = os.path.isfile(old_full_file_name)
+                is_dir = os.path.isdir(old_full_file_name)
+
+                if is_file:
+                    base_name = os.path.basename(old_full_file_name)
+                    new_full_file_name = os.path.join(os.path.dirname(old_full_file_name), str(index) + '.txt')
+                    print(new_full_file_name)
+                    os.rename(old_full_file_name, new_full_file_name)
+                elif is_dir and go_deep:
+                    SystemUtil.batch_rename_file(path=old_full_file_name)
+                    new_full_file_name = os.path.join(os.path.dirname(old_full_file_name), str(index))
+                    print(new_full_file_name)
+                    os.rename(old_full_file_name, new_full_file_name)
+                else:
+                    logging.error(ResultCode.CONDITION_NOT_CONFORM)
+
 
 if __name__ == '__main__':
     SystemUtil.batch_rename_file('/Users/luoyanjie/PycharmProjects/thor_handle')
