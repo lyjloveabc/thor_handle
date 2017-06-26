0027a7dca0b0042e09052762c363f8479b1d3ddf
luoyanjie <546223592@qq.com>
2017-06-26 15:12:53 +0800

    lyj    自动化；

---
+++ handle/itl/autoInit/adminEmployee2User/file/_20170626/ImportSomeUser_out_20170626.sql
@@ -0,0 +1,3 @@
+BEGIN;
+insert into user(gmt_create, gmt_modify, account, password, name, identity_card, nickname,sex, mobile, avatar_url, address, email, emergency_contact,emergency_mobile, last_login_time, last_login_ip, category_id,category_name, company_id, zone_id, zone_ids, is_on_job, departure_time,job_title_id, job_title_name, old_user_id) values (now(), now(), "13966021861", "74fd0ff378c433cfee7ce93143c106d9", "戴小妹", "360321198405241029", "戴小妹","F", "13966021861", "", "", "", "林中伟","13611980352", "1970-01-01 00:00:01", "127.0.0.1", "183","工程维修", "71", 24, "24", "1", "1970-01-01 00:00:01","0", "00/00/0000 00:00:00", "934");
+COMMIT;
--- handle/itl/autoInit/adminEmployee2User/importSomeUser.py
+++ handle/itl/autoInit/adminEmployee2User/importSomeUser.py
@@ -5,7 +5,7 @@ import os
 
 
 class ImportSomeUser:
-    DAY = '_20170619'
+    DAY = '_20170626'
     BASE_PATH = 'file/' + DAY + '/'
 
     def __init__(self):
