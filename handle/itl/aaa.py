88948911d98767e3d3b062795cf020c9686f2c07
luoyanjie <546223592@qq.com>
2017-07-13 12:59:03 +0800

    lyj    1、OK；

--- handle/itl/aaa.py
+++ handle/itl/aaa.py
@@ -1,3 +1,7 @@
-with open('aa.txt', 'r') as f:
-    for row in f.readlines():
-        print("INSERT INTO itl_user_task (zone_task_id, user_id) VALUES (" + str(row[:-1]) + ", 922);")
+# with open('aa.txt', 'r') as f:
+#     for row in f.readlines():
+#         print("INSERT INTO itl_user_task (zone_task_id, user_id) VALUES (" + str(row[:-1]) + ", 922);")
+
+for row in range(45, 95):
+    print("INSERT INTO itl_user_task(created_time, modified_time, zone_task_id, user_id) " \
+          "VALUES(now(), now(), " + str(row) + ", 922); ")
