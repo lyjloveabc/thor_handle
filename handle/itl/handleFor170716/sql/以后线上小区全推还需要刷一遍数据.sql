c3debd0632bcf830ed437383c8a84784b3440d81
luoyanjie <546223592@qq.com>
2017-07-18 10:37:14 +0800

    lyj    1、OK；

--- handle/itl/handleFor170716/sql/以后线上小区全推还需要刷一遍数据.sql
+++ handle/itl/handleFor170716/sql/以后线上小区全推还需要刷一遍数据.sql
@@ -1,10 +1,35 @@
 ## 更新现有小区的公司名称
+UPDATE zones SET company_id = "11" WHERE id = "1";
+UPDATE zones SET company_id = "1" WHERE id = "2";
+UPDATE zones SET company_id = "13" WHERE id = "5";
+UPDATE zones SET company_id = "9" WHERE id = "9";
+UPDATE zones SET company_id = "8" WHERE id = "10";
+UPDATE zones SET company_id = "12" WHERE id = "11";
+UPDATE zones SET company_id = "12" WHERE id = "12";
+UPDATE zones SET company_id = "10" WHERE id = "13";
+UPDATE zones SET company_id = "10" WHERE id = "14";
+UPDATE zones SET company_id = "10" WHERE id = "15";
+UPDATE zones SET company_id = "10" WHERE id = "16";
+UPDATE zones SET company_id = "10" WHERE id = "17";
+UPDATE zones SET company_id = "6" WHERE id = "18";
+UPDATE zones SET company_id = "9" WHERE id = "19";
+UPDATE zones SET company_id = "12" WHERE id = "20";
+UPDATE zones SET company_id = "3" WHERE id = "21";
+UPDATE zones SET company_id = "3" WHERE id = "22";
+UPDATE zones SET company_id = "7" WHERE id = "23";
+UPDATE zones SET company_id = "2" WHERE id = "24";
+UPDATE zones SET company_id = "4" WHERE id = "25";
+UPDATE zones SET company_id = "4" WHERE id = "26";
+UPDATE zones SET company_id = "4" WHERE id = "27";
+UPDATE zones SET company_id = "5" WHERE id = "28";
+UPDATE zones SET company_id = "9" WHERE id = "29";
+UPDATE zones SET company_id = "5" WHERE id = "30";
 
 ## 所有的人员更新公司
 UPDATE user LEFT JOIN zones ON zones.id = user.zone_id SET user.company_id = zones.company_id;
 
 ## 所有老校区的账单更新字段
-UPDATE bill SET is_checked=1, financial_income=ought_amount WHERE zone_id in ();
+UPDATE bill SET is_checked=1, financial_income=ought_amount where zone_id <= 30;
 
 ## 所有业务更新user_id
 UPDATE subscription_enter
