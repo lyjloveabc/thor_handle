d420c1353fd8581747b847a0c7b7a8fcafb54573
luoyanjie <546223592@qq.com>
2017-12-04 17:27:28 +0800

    lyj    1、OK；

--- handle/itl/h20171121/asu.sql
+++ handle/itl/h20171121/asu.sql
@@ -23,4 +23,25 @@ WHERE id IN
       )
       AND `gmt_start` < '2018-11-01 00:00:00';
 
+SELECT *
+FROM bill
+WHERE id IN (
+  SELECT bill_id
+  FROM `itl_parking_bill`
+  WHERE parking_id IN (SELECT id
+                       FROM `itl_parking`
+                       WHERE zone_id = 2 AND `name` = '7-35')
+)
+      AND `gmt_start` < '2018-08-01 00:00:00' AND `status` = 'NO_PAY';
+
+UPDATE bill
+SET `real_amount` = `ought_amount`, `gmt_modify` = now(), `gmt_pay` = now(), `remark` = '处理L', `status` = 'PAID', `financial_income` = 0.00
+WHERE id IN (SELECT bill_id
+             FROM `itl_parking_bill`
+             WHERE parking_id IN (SELECT id
+                                  FROM `itl_parking`
+                                  WHERE zone_id = 2 AND `name` = '7-36')) AND `gmt_start` < '2018-08-01 00:00:00' AND `status` = 'NO_PAY';
+
+
+
 COMMIT;
