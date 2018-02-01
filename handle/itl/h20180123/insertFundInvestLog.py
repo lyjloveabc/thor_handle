be489de602e8e5c3c47962ffa36bd23adf828005
luoyanjie <546223592@qq.com>
2018-02-01 10:50:58 +0800

    lyj    1、OK；

--- handle/itl/h20180123/insertFundInvestLog.py
+++ handle/itl/h20180123/insertFundInvestLog.py
@@ -5,13 +5,15 @@ import requests
 
 class InsertFundInvestLog:
     def __init__(self):
+        # self.ENV = 'http://127.0.0.1:7001'
         self.ENV = 'http://127.0.0.1:7001'
+        self.ENV_PROD = 'http://121.43.166.200:7001'
 
     def handle(self):
-        with open('fundInvestLog.json', 'r') as f:
+        with open('fundInvestLog_20180131.json', 'r') as f:
             data = json.load(f)
         for row in data:
-            response = requests.post(self.ENV + '/fundInvestLog/fundInvestLog', data=row)
+            response = requests.post(self.ENV_PROD + '/fundInvestLog/fundInvestLog', data=row)
             print(response.text)
 
 
--- handle/itl/h20180123/money.py
+++ handle/itl/h20180123/money.py
@@ -1,6 +1,14 @@
-a = 1000
+source = 1000.00
+n = 0
+count = 6
+some_time_sum = 0
+now = 994.59
 
-for x in range(1, 9):
-    a = a * 1.002
+while n < count:
+    this_time = source * (1.008 ** n)
+    some_time_sum += this_time
+    print(n + 1, this_time)
+    n += 1
 
-print(a)
+print(some_time_sum)
+print(some_time_sum - (now - 1500))
