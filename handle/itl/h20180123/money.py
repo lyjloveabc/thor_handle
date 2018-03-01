73a677d770d6265d634547a091d42b05235f04ce
luoyanjie <546223592@qq.com>
2018-03-01 15:11:59 +0800

    lyj    1、OK；

--- handle/itl/h20180123/money.py
+++ handle/itl/h20180123/money.py
@@ -1,15 +1,18 @@
 source = 1000.00
-n = 0
-count = 2
+n = 1
+count = 3
 some_time_sum = 0
-now = 967.60
+now = 1754.41
 other = 0
 
-while n < count:
-    this_time = source * (1.008 ** n)
+while n <= count:
+    this_time = source * (1.008 ** (n - 1))
     some_time_sum += this_time
-    print(n + 1, this_time)
+    print(n, this_time)
     n += 1
 
 print(some_time_sum)
 print(some_time_sum - (now - other))
+
+
+# 6134.65
