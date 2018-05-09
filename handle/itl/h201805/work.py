05b7d7cac6c145dccc50a7fe086bc33eb1b28b49
luoyanjie <546223592@qq.com>
2018-05-09 21:27:51 +0800

    lyj    1、OK；

--- handle/itl/h201805/work.py
+++ handle/itl/h201805/work.py
@@ -8,14 +8,24 @@ class Work:
         self.today = datetime.datetime.now()
         self.file_date_str = '_' + self.today.strftime('%Y%m%d')
 
-        self.ENV_IP = '127.0.0.1:8001'
+        # self.ENV_IP = '127.0.0.1:8001'
+        self.ENV_IP = 'prenewcloud.itianluo.cn'
         self.url = 'http://{ENV_IP}/work/workList.do?app=GJ&client=iPhone&version=1.6.6&outerType=STAFF&token=77f56c02a13b828aa80769cf495cf0fc&userId=35' \
             .format(ENV_IP=self.ENV_IP)
 
     def handle(self):
-        fz = {
+        task = 'TASK'
+        task_search_param = {
             'taskListFrom': 'TASK_CENTER'
         }
+
+        plan = 'PLAN'
+        plan_search_param = {
+
+        }
+
+        work_type = plan
+        search_param = plan_search_param
 
         param = {
             'app': 'GJ',
@@ -28,12 +38,10 @@ class Work:
             'zoneId': '1',
             'pageNum': '1',
             'pageSize': '20',
-            'workType': 'TASK',
-            'workListSearch': json.dumps(fz)
+            'workType': work_type,
+            'workListSearch': json.dumps(search_param)
         }
 
-        print(json.dumps(param))
-        print(self.url)
         headers = {'content-type': 'application/json'}
         response = requests.post(self.url, headers=headers, data=json.dumps(param))
         json_data = json.loads(response.text)
