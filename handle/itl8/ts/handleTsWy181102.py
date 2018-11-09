366d8b56a5f157abd4445cb64c2c12fed9aebf5c
luoyanjie <546223592@qq.com>
2018-11-09 15:38:36 +0800

    lyj    1、OK；

--- handle/itl8/ts/handleTsWy181102.py
+++ handle/itl8/ts/handleTsWy181102.py
@@ -3,6 +3,10 @@
 住宅物业费，出36个月（36个账单），2017年12-2018年11月的，审核，另外不审核；
 住宅公共维修费，出3年（3个账单），第一年的审核，其他不审核
 住宅能耗费，出1年，审核
+
+select
+id, period_name, date_format(period_start, '%Y-%m-%d'), date_format(period_end, '%Y-%m-%d')
+from subscription_period where zone_id = 76 and product_id = 43;
 """
 
 from utils.file.excel.readUtil import ReadUtil
@@ -14,6 +18,7 @@ INSERT INTO bill (id,
                   gmt_modify,
                   gmt_start,
                   gmt_end,
+                  house_info_id,
                   ought_amount,
                   product_id,
                   product_type_code,
@@ -24,64 +29,45 @@ INSERT INTO bill (id,
                   zone_id,
                   is_checked,
                   financial_income,
-                  sub_period_id,
-                  house_info_id
-                  )
+                  sub_period_id)
 VALUES 
-({b_id}, '{p_name}', now(), now(), "{gmt_start}"," {gmt_end}", {ought_amount}, 43, 'propertyFee', 7, NULL, 'NO_PAY', '{pre} {title}', 76, 0, {financial_income}, {p_id}, {house_info_id});
-
-INSERT INTO itl_business_bill (id, created_time, modified_time, zone_id, business_id, bill_id, type, last_operator)
-VALUES ({biz_id}, now(), now(), 76, {s_id}, {b_id}, 'STORE', 999999999);
+({b_id}, '{p_name}', now(), now(), "{start}"," {end}", {house_info_id}, {ought_amount}, 43, 'propertyFee', 7, NULL, 'NO_PAY', '{title}', 76, {is_checked}, {financial_income}, {p_id});
 """
 
-field_index = {
-    's_id': 0,
-    'money': 2,
-}
+data = ReadUtil.read_file(
+    '物业用房账单明细_data.xls',
+    {
+        'id': 0,
+        'house': 1,
+        'building': 2,
+        'door': 3,
+        'wyf': 8,
+        'ggf': 9,
+        'nhf': 10,
+    }
+)
 
-file_name = '天水.xlsx'
-data = ReadUtil.read_file(file_name, field_index)
+sp = list()
+with open('wyf_p.txt', 'r') as f:
+    for line in f.readlines():
+        d = line.replace('\n', '').split(',')
+        sp.append(d)
 
-b_id = 478026
-biz_id = 24257
-p_name = [
-    {'n': '商铺物业费 2017年12月', 'p': 382, 'start': '2017-12-01', 'end': '2017-12-31'},
-    {'n': '商铺物业费 2018年01月', 'p': 383, 'start': '2018-01-01', 'end': '2018-01-31'},
-    {'n': '商铺物业费 2018年02月', 'p': 384, 'start': '2018-02-01', 'end': '2018-02-28'},
-    {'n': '商铺物业费 2018年03月', 'p': 385, 'start': '2018-03-01', 'end': '2018-03-31'},
-    {'n': '商铺物业费 2018年04月', 'p': 386, 'start': '2018-04-01', 'end': '2018-04-30'},
-    {'n': '商铺物业费 2018年05月', 'p': 387, 'start': '2018-05-01', 'end': '2018-05-31'},
-    {'n': '商铺物业费 2018年06月', 'p': 388, 'start': '2018-06-01', 'end': '2018-06-30'},
-    {'n': '商铺物业费 2018年07月', 'p': 389, 'start': '2018-07-01', 'end': '2018-07-31'},
-    {'n': '商铺物业费 2018年08月', 'p': 390, 'start': '2018-08-01', 'end': '2018-08-31'},
-    {'n': '商铺物业费 2018年09月', 'p': 391, 'start': '2018-09-01', 'end': '2018-09-30'},
-    {'n': '商铺物业费 2018年10月', 'p': 392, 'start': '2018-10-01', 'end': '2018-10-31'},
-    {'n': '商铺物业费 2018年11月', 'p': 393, 'start': '2018-11-01', 'end': '2018-11-30'},
-]
-p_id = 382
-
-sssss = dict()
-with open('hahahahha.txt', 'r') as f:
-    for row in f.readlines():
-        row = row.replace('\n', '')
-        aa = row.split(',')
-        sssss[aa[0]] = aa[1]
-
+b_id = 483778
 for row in data:
-    for pn in p_name:
+    for p in sp:
         print(
             sql.format(
                 b_id=b_id,
-                gmt_start=pn['start'],
-                gmt_end=pn['end'],
-                p_name=pn['n'],
-                ought_amount=row['money'],
-                pre=sssss[str(int(float(row['s_id'])))],
-                title=pn['n'],
-                financial_income=row['money'],
-                biz_id=biz_id, s_id=row['s_id'],
-                p_id=pn['p'],
+                p_name=p[1],
+                start=p[2],
+                end=p[3],
+                house_info_id=row['id'],
+                ought_amount=row['wyf'],
+                title=row['house'] + "-" + row['building'] + "-" + row['door'] + " " + p[1],
+                is_checked=(1 if int(p[0]) <= 156 else 0),
+                financial_income=row['wyf'],
+                p_id=p[0],
             )
         )
         b_id += 1
-        biz_id += 1
