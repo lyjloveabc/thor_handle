fa655a23b0cad9b62dcd48732c6b51055b74dec9
luoyanjie <546223592@qq.com>
2019-05-05 19:10:40 +0800

    lyj    1、OK；

---
+++ handle/itl19/wealth/priceTypeTxt.py
@@ -0,0 +1,29 @@
+sql_house = 'UPDATE house_info SET price_type_name = "{price_type_name}" WHERE zone_id = {zone_id} AND id = {id};'
+sql_store = 'UPDATE itl_store SET price_type_name = "{price_type_name}" WHERE zone_id = {zone_id} AND id = {id};'
+sql_parking = 'UPDATE itl_parking SET price_type_name = "{price_type_name}" WHERE zone_id = {zone_id} AND id = {id};'
+
+file_name = 'file/住宅单价类型订正.csv'
+
+data = list()
+with open(file_name, 'r') as f:
+    for row in f.readlines():
+        row = row.replace('\n', '')
+        arr = row.split(',')
+        data.append({
+            'id': arr[0],
+            'zone_id': arr[1],
+            'price_type': arr[2],
+        })
+
+print('数据总量：', len(data))
+
+need = list()
+
+for row in data:
+    need.append(
+        sql_house.format(price_type_name=row['price_type'], zone_id=row['zone_id'], id=row['id'])
+    )
+
+with open(file_name.replace('.', '_' + '_out.txt'), 'a') as f:
+    for row in need:
+        f.write(row + '\n')
