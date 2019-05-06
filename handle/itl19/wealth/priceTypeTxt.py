52157df2363d48f1ddbab87441ed2c7a896d04a8
luoyanjie <546223592@qq.com>
2019-05-06 21:55:30 +0800

    lyj    1、OK；

--- handle/itl19/wealth/priceTypeTxt.py
+++ handle/itl19/wealth/priceTypeTxt.py
@@ -2,17 +2,16 @@ sql_house = 'UPDATE house_info SET price_type_name = "{price_type_name}" WHERE  
 sql_store = 'UPDATE itl_store SET price_type_name = "{price_type_name}" WHERE  id = {id};'
 sql_parking = 'UPDATE itl_parking SET price_type_name = "{price_type_name}" WHERE  id = {id};'
 
-file_name = 'file/商铺单价类型订正.txt'
+file_name = 'file/linshi.txt'
 
 data = list()
 with open(file_name, 'r') as f:
     for row in f.readlines():
         row = row.replace('\n', '')
-        arr = row.split('	')
+        arr = row.split(' ')
         data.append({
             'id': arr[0],
-            'zone_id': arr[1],
-            'price_type': arr[2],
+            'price_type': arr[1] if len(arr) > 1 else '',
         })
 
 print('数据总量：', len(data))
